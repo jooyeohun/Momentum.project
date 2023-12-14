df85acef02c250be7fe5508651c9f722b609b5a9
jooyeohun <153431496+jooyeohun@users.noreply.github.com>
2023-12-14 23:24:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,32 @@ const loginInput = document.querySelector("#login-form input");
 const grerting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function loginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("USERNAME_KEY", username);
+    painGreetings(username);
+}
+
+function painGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener("submit", loginSubmit);
+
+const saveUsername = localStorage.getItem("USERNAME_KEY");
+
+if(saveUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", loginSubmit);
+} else {
+    painGreetings(saveUsername);
+}
+
+
 
 
