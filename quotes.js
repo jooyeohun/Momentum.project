42b88ebbc7dca31905146cfb2462f2fb7e685105
jooyeohun <153431496+jooyeohun@users.noreply.github.com>
2023-12-19 22:54:43 +0900

    Add files via upload

--- quotes.js
+++ quotes.js
@@ -50,9 +50,9 @@ const quotes = [
 ];
 
 const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+const arthor = document.querySelector("#quote span:last-child");
 
-aysQuotes = quotes[Math.floor(Math.random() * quotes.length)]
+todaysQuote = quotes[Math.floor(Math.random() * quotes.length)]
 
 quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+arthor.innerText = todaysQuote.arthor;
